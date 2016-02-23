e8974620b39ed6494c23a8a3de0c160f60876962
Loquacious <Loquacious@users.noreply.github.com>
2016-02-23 07:58:51 -0500

    Multiple routes

--- scripts/script.js
+++ scripts/script.js
@@ -1,22 +1,41 @@
-function game() {
+function game () {
 
 // Don't indent first-level code inside of the function.
 // It takes up more indentation over-all and is unnecessary.
 
 // Actual game variables
-var day = 0,
-	gameover = false;
+var gameover = false,
+	day = 0,
+	location;
 // Player well-being
 var health = 100,
 	hunger = 0,
 	thirst = 0,
 	sleep = 100;
+// Player info
+var attack = 0,
+	defense = 0,
+	speed = 0,
+	inventory = {
+		food: 25,
+		water: 25,
+		scythe: 1,
+		shovel: 1,
+		sleeping_bag: 1,
+
+	};
+// Main Choice
+event('You\'re gathering materials when some rustling\'s heard. It\'s definitely not safe. You have to go now. You gather your supplies. You have a choice to go to the [left] or the [right].', [function () {
+	location = 'woods';
+}, function () {
+	location = 'wetlands';
+}]);
 // Introduction
-alert('You wake up in a clearing in the woods. You look around and see \
-ruins. What was once a thriving village reduced to a few smoldering splinters. \
-Wait! what\'s that? You found a scroll. You read the scroll. It says, "What is \
-thriving now shall be destroyed (lots of mud covers this portion) Ancient race \
-of the dwarves". The rest is burnt.');
+if (location === 'woods') {
+	alert('You wake up in a clearing in the woods. You look around and see ruins. What was once a thriving village reduced to a few smoldering splinters. Wait! what\'s that? You found a scroll. You read the scroll. It says, "What is thriving now shall be destroyed (lots of mud covers this portion) Ancient race of the dwarves". The rest is burnt.');
+} else {
+	alert('You enter a wetlands plain. "That was weird. Let\'s take stock. We have plenty of food and fresh water and ... oh! What looks like a farmer\'s scythe and a shovel, plus a sleeping bag. Wasn\'t that lucky? We should probably build a shelter, the wetlands aren\'t the safest place." You spent the rest of the day building a makeshift shelter using tall grass and a pit. it looks pretty hidden.');
+}
 // Real beginning
 while (!gameover) {
 	day++;
