f4848904ce71a439ee7e2f17a455f97e9493d3bb
LordoftheWild <harshnp02@gmail.com>
2016-02-24 14:36:37 -0500

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -33,7 +33,7 @@ event('You\'re gathering materials when some rustling\'s heard. It\'s definitely
 }]);
 // Introduction
 if (location === 'ruins') {
-	alert('You wake up in a clearing in the woods. You look around and see ruins. What was once a thriving village reduced to a few smoldering splinters. Wait! what\'s that? You found a scroll. You read the scroll. It says, "What is thriving now shall be destroyed (lots of mud is stuck to this portion) Ancient race of the dwarves". The rest is burnt.');
+	alert('You wake up in a clearing in the woods. You look around and see ruins. What was once a thriving village reduced to a few smoldering splinters. Wait! what is that ? You found a scroll. You read the scroll. It says, "What is thriving now shall be destroyed (lots of mud is stuck to this portion) Ancient race of the dwarves". The rest is burnt.');
 } else {
 	if (location === 'woods') {
 	alert('you decide to hide in the woods. Alright, that was almost too close. Let\'s take stock of our supplies. you have some food, some water and...oh! an axe! nice! you spend the rest of the day building a very crude treehouse. hmm...looks like the sun is setting. goodnight.)
