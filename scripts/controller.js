97cfb2403628cc8c8b627052aac0add655ac2519
LordoftheWild <harshnp02@gmail.com>
2016-03-08 10:49:13 -0500

    play button?
    would this add a play button?
    I don't know
    maybe
    maybe not

---
+++ scripts/controller.js
@@ -0,0 +1,16 @@
+
+window.onload = function () { 
+ 	var alpha = document.createElement('sup'), 
+ 		play = document.createElement('a'), 
+ 		header = document.getElementsByClassName('page-header')[0]; 
+ 	beta.innerHTML = 'ALPHA'; 
+ 	play.href = '#'; 
+ 	play.className = 'btn'; 
+ 	play.innerHTML = 'Play Game'; 
+ 	play.onclick = function () { 
+ 		eternal-forest(); 
+ 		return false; 
+ 	}; 
+ 	play.appendChild(alpha); 
+ 	header.appendChild(play); 
+ }; 
