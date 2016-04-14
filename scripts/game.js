66ee527e9149761d18eae3a77bcdbb1b23bbca2a
Harsh Patel <harshnp02@gmail.com>
2016-04-14 10:04:54 -0400

    game
    this is the file where we make the actual RPG style game
    DO NOT LOAD onto the html yet
    added the stats

---
+++ scripts/game.js
@@ -0,0 +1,15 @@
+//create game code here
+//gameloop
+
+//player stats
+var health = 100;
+var energy = 10;
+var water level = 100; //how much negetive thirst you have
+var attack = 1;
+var speed = 5;
+var intelligence = 5;
+//other game variables
+
+//player move code
+
+//rest of game
